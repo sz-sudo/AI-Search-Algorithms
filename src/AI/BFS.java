b676fd16eb5a90472beaf0475632cab94b386fce
sz <shoayb.zaynali@yahoo.com>
2022-02-23 22:01:51 +0330

    minor changes

--- src/AI/BFS.java
+++ src/AI/BFS.java
@@ -13,25 +13,33 @@ public class BFS {
         Queue<Node> frontier = new LinkedList<Node>();
         Hashtable<String, Boolean> inFrontier = new Hashtable<>();
         Hashtable<String, Boolean> explored = new Hashtable<>();
+
         if (startNode.isGoal()) {
             System.out.println("score : " + startNode.sum);
             printResult(startNode, 0);
             return;
         }
+
         frontier.add(startNode);
         inFrontier.put(startNode.hash(), true);
+
         while (!frontier.isEmpty()) {
+
             Node temp = frontier.poll();
             inFrontier.remove(temp.hash());
             explored.put(temp.hash(), true);
+
             ArrayList<Node> children = temp.successor();
+
             for (Node child : children) {
                 if (!(inFrontier.containsKey(child.hash())) && !(explored.containsKey(child.hash()))) {
+
                     if (child.isGoal()) {
                         printResult(child, 0);
                         System.out.println(child.sum);
                         return;
                     }
+
                     frontier.add(child);
                     inFrontier.put(child.hash(), true);
                 }
